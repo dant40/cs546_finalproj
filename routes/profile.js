db94006026c9e54bfe2c8c81727beb10dc1ad292
maxlep <mlepkows@stevens.edu>
2019-12-14 11:32:50 -0500

    Add layout with nav header

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,7 @@ const data = require('../data');
 const users = data.users;
 
 router.get('/profile', async(req, res) => {
-    res.render('profile/profile');
+    res.render('profile/profile', {layout: 'nav'});
 });
 
 module.exports = router;
