257c83fb42786b0187a7a9a67bfb54773c05a75a
xxdavis97 <xxdavis97@gmail.com>
2019-12-15 14:43:05 -0500

    Fixed routing

--- routes/profile.js
+++ routes/profile.js
@@ -3,13 +3,19 @@ const router = express.Router();
 const data = require('../data');
 const users = data.users;
 
-function getPageTitle(req) {
-    return `${req.session.username}'s Profile`;
+function getPageTitle(username) {
+    return `${username}'s Profile`;
 }
 
 router.get('/profile', async(req, res) => {
-  let user = await users.get(req.session.username);
-  res.render('profile/profile', {layout: 'nav', title: getPageTitle(req), profile: user});
+  let user = await users.getByUsername(req.session.username);
+  res.render('profile/profile', {layout: 'nav', title: getPageTitle(req.session.username), profile: user});
+});
+
+router.get('/profile/:user', async(req, res) => {
+  console.log()
+  let user = await users.getByUsername((req.params.user));
+  res.render('profile/profile', {layout: 'nav', title: getPageTitle(req.params.user), profile: user});
 });
 
 module.exports = router;
