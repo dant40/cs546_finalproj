2705d8960324ea722e89985ad01f0ae4a2f1428d
Zachary Talarick <ztalarick@gmail.com>
2019-12-16 16:04:41 -0500

    Finished friend functionality please check for bugs

--- routes/friend.js
+++ routes/friend.js
@@ -16,39 +16,41 @@ function getPageTitle(username) {
 
 
 router.get('/friends', async (req, res) => {
-  //console.log("made it here");
-  let friendsList = await users.getByUsername(req.session.username).friends;
+  let currentUser = await users.getByUsername(req.session.username);
+  //currentUser.profile.friends -> [list of usernames], do this to get friendsList for displayname
+  let friendsList = [];
+  let usernamesList = currentUser.profile.friends;
+  let friend = " "
+  for(let i = 0; i < usernamesList.length; i++){
+    try {
+      friend = await users.getByUsername(usernamesList[i]);
+    } catch (e) {
+      console.log(e); //this is called when you have a friend in your friendsList that is not a User
+      friend = " ";
+    } finally {
+      if(friend != " "){
+        friendsList.push(friend);
+      }
+    }
+  }
   res.render('friends/friends', {layout: 'nav', title:getPageTitle(req.session.username), friends: friendsList});
 });
 
 router.post('/add', async (req, res) => {
-  //req.body.addFriend if submited through form
-  //res.redirect('/add/' + req.body.addFriend);
+  let user2 = req.body.addFriend; //if added from form
+  if(!user2) user2 = req.body.userName //if added from the button on their profile page
   try {
-    let user = await users.addFriend(await users.getByUsername(req.session.username), await users.getByUsername(req.body.addFriend));
+    let user = await users.addFriend(req.session.username, user2);
   } catch (e) { //something goes wrong with adding
     console.log(e);
   } finally {
-    //console.log("finished the function");
     res.redirect('/friends');
   }
 });
 
-// router.post('/add/:userName', async (req, res) =>  {
-//   console.log("made it here");
-//   try {
-//     let user = await users.addFriend(await users.getByUsername(req.session.username), await users.getByUsername(req.params.userName));
-//   } catch (e) { //something goes wrong with adding
-//     console.log(e);
-//   } finally {
-//     console.log("finished the function");
-//     res.redirect('/friends');
-//   }
-// });
-
-router.post('/remove/:userName', async (req, res) =>  {
+router.post('/remove', async (req, res) =>  {
   try {
-    let user = await users.removeFriend(await users.getByUsername(req.session.username), await users.getByUsername(req.params.userName));
+    let user = await users.removeFriend(req.session.username, req.body.userName);
   } catch (e) { //something goes wrong with removing
     console.log(e);
   } finally {
