0cdbd982ef213bece7bb25c897010adda2401294
xxdavis97 <xxdavis97@gmail.com>
2019-12-15 21:30:11 -0500

    posts

--- data/users.js
+++ data/users.js
@@ -155,7 +155,7 @@ async function deletePostFromUser(id,post_id){
 //doesn't enforce anything on the post as of now
 
     var col = await users();
-
+    console.log(id, post_id);
     const updateInfo = await col.updateOne({ _id: ObjectID(id) }, {$pull : {"profile.posts":  {_id: post_id } } });
     if (updateInfo.modifiedCount === 0) {
         throw new Error ( "Could not perform post deletion successfully");
